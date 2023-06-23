b84c58480b897e72a3adbba9f56bc23b95bc9852
Danila Lebedkin <lebedkin_danila@mail.ru>
2023-06-23 16:31:34 +0300

    resize btns on small screens, add new module in togglescript

--- scripts/toggleScript.js
+++ scripts/toggleScript.js
@@ -84,3 +84,50 @@ function toggleActiveBtn(event, className) {
     });
 
 }
+
+
+const handleWindowResize = () => {
+    const mobileMarkup = `
+    <div class=" d-md-flex mb-2 d-lg-none">
+
+                <div class="d-flex w-75 row">
+                  <div class="d-flex  gap-2 " >
+                    <button class="btn active typeOfShrinkBtn" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">ПОФ</button>
+                    <button class="btn typeOfShrinkBtn" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">ПОФ-Перфорированный</button>
+                  </div>
+            
+                  <div class="d-flex  gap-2  ">
+                    <button class="btn typeOfShrinkBtn" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">ПВХ-Т</button>
+                    <button class="btn typeOfShrinkBtn" id="nav-lite-tab" data-bs-toggle="tab" data-bs-target="#nav-lite" type="button" role="tab" aria-controls="nav-lite" aria-selected="false" >ПВХ-Т Lite</button>
+                  </div>
+            
+                  <div class="d-flex gap-2  ">
+                    <button class="btn typeOfShrinkBtn" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">ПВД</button>
+                    <button class="btn typeOfShrinkBtn" id="nav-lite-tab" data-bs-toggle="tab" data-bs-target="#nav-lite" type="button" role="tab" aria-controls="nav-lite" aria-selected="false">БОПП</button>
+                  </div>
+                </div>
+               
+              </div>
+    `;
+    const desktopMarkup = `
+    <div class="d-none d-lg-flex  justify-content-center gap-2 flex-wrap">
+                <button class="btn active typeOfShrinkBtn" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">ПОФ</button>
+                <button class="btn typeOfShrinkBtn" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">ПОФ-Перфорированный</button>
+                <button class="btn typeOfShrinkBtn" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">ПВХ-Т</button>
+                <button class="btn typeOfShrinkBtn" id="nav-lite-tab" data-bs-toggle="tab" data-bs-target="#nav-lite" type="button" role="tab" aria-controls="nav-lite" aria-selected="false" >ПВХ-Т Lite</button>
+                <button class="btn typeOfShrinkBtn" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">ПВД</button>
+                <button class="btn typeOfShrinkBtn" id="nav-lite-tab" data-bs-toggle="tab" data-bs-target="#nav-lite" type="button" role="tab" aria-controls="nav-lite" aria-selected="false">БОПП</button>
+              </div>
+    `;
+    const windowWidth = window.innerWidth
+    if (windowWidth >= 960) {
+        document.querySelector(".nav-type-of-shrink").innerHTML = desktopMarkup
+    } else {
+      document.querySelector(".nav-type-of-shrink").innerHTML = mobileMarkup
+    }
+  }
+  
+  window.addEventListener('resize', handleWindowResize)
+  
+  // запускаем один раз, чтобы сразу проверить начальную ширину окна
+  handleWindowResize()
